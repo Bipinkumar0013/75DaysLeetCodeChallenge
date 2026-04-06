75c061ad6764105656b74e2383ad93b636832439
Bipin kumar prajapati <bkbipin231006@gmail.com>
2026-04-06 19:44:55 +0530

    Time: 0 ms (100%), Space: 42.8 MB (29.06%) - LeetHub

--- 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.java
+++ 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.java
@@ -6,35 +6,68 @@ Stack<Integer> helper = new Stack<>();
     }
     
     public void push(int x) {
+       while(st.size()>0){
+            helper.push(st.pop());
+        }
         st.push(x);
-    }
-    
-    public int pop() {
-       while (st.size() > 1) {
-            helper.push(st.pop());
-        }
-        int front = st.pop(); 
-        while (helper.size() > 0) {
-            st.push(helper.pop());
-        }
-        return front;
-    }
-    
-    public int peek() {
-        while(st.size()>1){
-            helper.push(st.pop());
-        }
-        int front = st.peek();
          while(helper.size()>0){
             st.push(helper.pop());
         }
-        return front;
+    
+    }
+    
+    public int pop() {
+     return  st.pop();
+    }
+    
+    public int peek() {
+       return st.peek();
     }
     
     public boolean empty() {
        return (st.size()==0) ;
     }
 }
+
+
+
+// class MyQueue {
+// Stack<Integer> st = new Stack<>();
+// Stack<Integer> helper = new Stack<>();
+//     public MyQueue() {
+     
+//     }
+    
+//     public void push(int x) {
+//         st.push(x);
+//     }
+    
+//     public int pop() {
+//        while (st.size() > 1) {
+//             helper.push(st.pop());
+//         }
+//         int front = st.pop(); 
+//         while (helper.size() > 0) {
+//             st.push(helper.pop());
+//         }
+//         return front;
+//     }
+    
+//     public int peek() {
+//         while(st.size()>1){
+//             helper.push(st.pop());
+//         }
+//         int front = st.peek();
+//          while(helper.size()>0){
+//             st.push(helper.pop());
+//         }
+//         return front;
+//     }
+    
+//     public boolean empty() {
+//        return (st.size()==0) ;
+//     }
+// }
 
 /**
  * Your MyQueue object will be instantiated and called as such:
