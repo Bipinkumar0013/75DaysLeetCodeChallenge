583337bee5f3881e44e47f3847b43169669548bc
Bipin kumar prajapati <bkbipin231006@gmail.com>
2026-08-17 16:21:22 +0530

    Time: 0 ms (100%), Space: 43.9 MB (71.73%) - LeetHub

--- 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.java
+++ 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.java
@@ -15,25 +15,40 @@
  */
 class Solution {
     public void flatten(TreeNode root) {
-        ArrayList<TreeNode> arr = new ArrayList<>();
-        dfs(root , arr);
-        for(int i = 0 ; i< arr.size()-1; i++){
-            TreeNode a = arr.get(i);
-            TreeNode b = arr.get(i+1);
-            a.right = b;
-            a.left = null;
-
-        }
-        if (arr.size() == 0) return;
-        TreeNode last = arr.get(arr.size()-1);
-        last.left = null;
-        last.right = null;
-
+        // disconnect
+        if(root == null) return;
+        TreeNode lst = root.left;
+        TreeNode rst = root.right;
+        root.left = null;
+        root.right = null;
+        // flatten lst and rsr
+        flatten(lst);
+        flatten(rst);
+        //Travel lst to the last
+        TreeNode last = root;
+        root.right =lst;
+        while(last.right!=null) last=last.right;
+        last.right = rst;
     }
-    public void dfs(TreeNode root , ArrayList<TreeNode> arr){
-        if(root == null) return ;
-        arr.add(root);
-        dfs(root.left , arr);
-        dfs(root.right , arr);
-     }
 }
+        // ArrayList<TreeNode> arr = new ArrayList<>();
+    //     dfs(root , arr);
+    //     for(int i = 0 ; i< arr.size()-1; i++){
+    //         TreeNode a = arr.get(i);
+    //         TreeNode b = arr.get(i+1);
+    //         a.right = b;
+    //         a.left = null;
+
+    //     }
+    //     if (arr.size() == 0) return;
+    //     TreeNode last = arr.get(arr.size()-1);
+    //     last.left = null;
+    //     last.right = null;
+
+    // }
+    // public void dfs(TreeNode root , ArrayList<TreeNode> arr){
+    //     if(root == null) return ;
+    //     arr.add(root);
+    //     dfs(root.left , arr);
+    //     dfs(root.right , arr);
+    //  }
