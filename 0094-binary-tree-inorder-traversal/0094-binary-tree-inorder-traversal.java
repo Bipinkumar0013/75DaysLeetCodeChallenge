a970d4555ef3582b7cb603c17ba48a5a2bfd3259
Bipin kumar prajapati <bkbipin231006@gmail.com>
2026-08-24 22:59:29 +0530

    Time: 0 ms (100%), Space: 43.1 MB (70.89%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.java
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.java
@@ -15,27 +15,50 @@
  */
 class Solution {
     public List<Integer> inorderTraversal(TreeNode root) {
-       ArrayList<Integer> ans = new ArrayList<>();
-       Stack<TreeNode> st = new Stack<>();
-       TreeNode curr = root;
-       while(st.size()>0 ||curr!=null){
-        if(curr!=null){
-          if(curr.left!= null){
-            st.push(curr);
-            curr = curr.left;
+      ArrayList<Integer> ans = new ArrayList<>();
+      TreeNode curr = root;
+      while(curr!=null){
+        if(curr.left!=null){//predecessor lagega  
+         TreeNode pred = curr.left;
+        while(pred.right!=null && pred.right != curr ){
+            pred = pred.right;
         }
-          else{
+        if(pred.right == null){
+            pred.right = curr;
+            curr = curr.left;
+        }else{//pred.rigth == curr
+        pred.right = null;
+        ans.add(curr.val);
+        curr = curr.right;
+        }
+        
+        }else{//predecessor nhi lagega
             ans.add(curr.val);
             curr = curr.right;
         }
-        }
-        else{
-             TreeNode top = st.pop();
-             ans.add(top.val);
-             curr = top.right;
-
-        }
-       } 
-       return ans;
+      }
+      return ans;
     }
 }
+//  ArrayList<Integer> ans = new ArrayList<>();
+//        Stack<TreeNode> st = new Stack<>();
+//        TreeNode curr = root;
+//        while(st.size()>0 ||curr!=null){
+//         if(curr!=null){
+//           if(curr.left!= null){
+//             st.push(curr);
+//             curr = curr.left;
+//         }
+//           else{
+//             ans.add(curr.val);
+//             curr = curr.right;
+//         }
+//         }
+//         else{
+//              TreeNode top = st.pop();
+//              ans.add(top.val);
+//              curr = top.right;
+
+//         }
+//        } 
+//        return ans;}
